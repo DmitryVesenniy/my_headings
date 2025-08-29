79156187ef04d203dea8d31d387991f42f1cb05a
dmitrijvesennij <press8328@protonmail.com>
2025-08-29 17:24:42 +0300

    init

---
+++ internal/server/routes.go
@@ -0,0 +1,54 @@
+package server
+
+import (
+	"encoding/json"
+	"html/template"
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/cors"
+)
+
+type Headers struct {
+	Header string
+	Value  string
+}
+
+func (s *Server) RegisterRoutes() http.Handler {
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+
+	r.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"https://*", "http://*"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type"},
+		AllowCredentials: true,
+		MaxAge:           300,
+	}))
+
+	r.Get("/", s.HelloWorldHandler)
+
+	r.Get("/health", s.healthHandler)
+
+	return r
+}
+
+func (s *Server) HelloWorldHandler(w http.ResponseWriter, r *http.Request) {
+	headers := make([]Headers, 0)
+
+	for header, value := range r.Header {
+		headers = append(headers, Headers{
+			Header: header,
+			Value:  value[0],
+		})
+	}
+
+	tmpl := template.Must(template.ParseFiles("templates/index.html"))
+	tmpl.Execute(w, headers)
+}
+
+func (s *Server) healthHandler(w http.ResponseWriter, r *http.Request) {
+	jsonResp, _ := json.Marshal(s.db.Health())
+	_, _ = w.Write(jsonResp)
+}
