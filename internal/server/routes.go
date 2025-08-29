2a70c3e9fa2db666bbe15c3d0204a76dd9c2d0f6
dmitrijvesennij <press8328@protonmail.com>
2025-08-29 19:08:26 +0300

    feat: add ip to user info

--- internal/server/routes.go
+++ internal/server/routes.go
@@ -3,7 +3,9 @@ package server
 import (
 	"encoding/json"
 	"html/template"
+	"net"
 	"net/http"
+	"strings"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
@@ -13,6 +15,34 @@ import (
 type Headers struct {
 	Header string
 	Value  string
+}
+
+type UserInfo struct {
+	Headers []Headers
+	IP      string
+}
+
+func getIP(r *http.Request) string {
+	// Проверяем заголовок X-Forwarded-For (может содержать список IP)
+	ip := r.Header.Get("X-Forwarded-For")
+	if ip != "" {
+		// Берём первый IP из списка
+		ips := strings.Split(ip, ",")
+		return strings.TrimSpace(ips[0])
+	}
+
+	// Проверяем заголовок X-Real-IP
+	ip = r.Header.Get("X-Real-IP")
+	if ip != "" {
+		return ip
+	}
+
+	// Используем RemoteAddr (формат: IP:port)
+	ip, _, err := net.SplitHostPort(r.RemoteAddr)
+	if err != nil {
+		return r.RemoteAddr // fallback
+	}
+	return ip
 }
 
 func (s *Server) RegisterRoutes() http.Handler {
@@ -40,12 +70,19 @@ func (s *Server) HelloWorldHandler(w http.ResponseWriter, r *http.Request) {
 	for header, value := range r.Header {
 		headers = append(headers, Headers{
 			Header: header,
-			Value:  value[0],
+			Value:  strings.Join(value, ", "),
 		})
 	}
 
+	ip := getIP(r)
+
+	userInfo := UserInfo{
+		Headers: headers,
+		IP:      ip,
+	}
+
 	tmpl := template.Must(template.ParseFiles("templates/index.html"))
-	tmpl.Execute(w, headers)
+	tmpl.Execute(w, userInfo)
 }
 
 func (s *Server) healthHandler(w http.ResponseWriter, r *http.Request) {
